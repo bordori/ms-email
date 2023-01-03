afa327a83d292f034602b60078df37f1abcd6709
jean <jeanbordori@gmail.com>
2023-01-03 20:21:45 -0300

    commit inicial

---
+++ src/main/java/com/email/msemail/models/EmailModel.java
@@ -0,0 +1,36 @@
+package com.email.msemail.models;
+
+import com.email.msemail.enums.StatusEmail;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+
+    private String ownerRef;
+
+    private String emailFrom;
+
+    private String emailTo;
+
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    private LocalDateTime sendDateEmail;
+
+    private StatusEmail statusEmail;
+
+
+
+
+}
